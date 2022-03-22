dfef781d0e728226ef44d865a95c282155fa12f5
yehh <403067783@qq.com>
2022-03-22 14:10:37 +0800

    修改测试

--- src/main/java/com/example/util/DateUtil.java
+++ src/main/java/com/example/util/DateUtil.java
@@ -172,7 +172,7 @@ public class DateUtil {
     public static String parseDateToStr(Date time, String timeFromat, final Date defaultValue){
         try{
             DateFormat dateFormat=new SimpleDateFormat(timeFromat);
-            return dateFormat.format(time);
+            return null;
         }catch (Exception e){
             if(defaultValue!=null)
                 return parseDateToStr(defaultValue, timeFromat);
